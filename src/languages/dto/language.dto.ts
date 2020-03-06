06c4cfbf2e261f0c084b1c1ef4044110cc998852
Nexoid <marvin.anfray@gmail.com>
2020-03-06 13:36:45 +0100

    fix(languages): remove needless child dto entry

--- src/languages/dto/language.dto.ts
+++ src/languages/dto/language.dto.ts
@@ -1,17 +1,13 @@
-import { LanguageStackDto } from './stack.dto';
-
 export class LanguageDto {
   id: string;
   name: string;
   url: string;
   icon: string;
-  stacks?: LanguageStackDto[];
 
-  constructor(id: string, icon: string, name: string, url: string, stacks?: LanguageStackDto[]) {
+  constructor(id: string, icon: string, name: string, url: string) {
     this.id = id;
     this.icon = icon;
     this.name = name;
     this.url = url;
-    this.stacks = stacks ?? [];
   }
 }
