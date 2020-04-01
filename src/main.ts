8f677104bb3a33b7cd20e0981411d1a323eef0ee
Nexoid <marvin.anfray@gmail.com>
2020-04-01 21:21:36 +0200

    feat(icons): add static icons folder

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const options = new DocumentBuilder()
     .setTitle('Stacks API')
     .setDescription('Stacks API description')
     .setVersion('1.0')
     .build();
+  app.useStaticAssets(join(__dirname, '..', 'data/icons'), { prefix: '/icons' });
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
